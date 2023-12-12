7d83735aba25ec93032f9f58095c27c077a11f20
Piotr Frankowski <piotr@f-dev.io>
2023-12-12 09:42:06 +0100

    add dtk in config example

--- config.example.js
+++ config.example.js
@@ -1,6 +1,7 @@
 module.exports = {
   API_URL: 'https://api.rhino.fi',
   PRIVATE_KEY: '',
+  DTK: '',
   PAIR: 'CDETI:ETH',
   ALCHEMY_URL: 'https://eth-mainnet.alchemyapi.io/v2/YOUR_ALCHEMY_KEY_HERE',
   DEPOSIT_TOKEN: 'CDETI',
